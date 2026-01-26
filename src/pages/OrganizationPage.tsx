f779b8f24d0b9c44c8a9a4c6730aa9f894779046
DaraWen <darawyd@gmail.com>
2026-01-26 12:40:25 -0600

    created orgnaization page

---
+++ src/pages/OrganizationPage.tsx
@@ -0,0 +1,31 @@
+import { roles } from "../data/roles";
+import "./OrganizationPage.css";
+
+export default function OrganizationPage() {
+    return (
+        <main className="org">
+            <h2>Leadership and Management</h2>
+
+            <div
+                className="org-table"
+                role="table"
+                aria-label="Leadership and Management"
+            >
+                {roles.map((r, idx) => (
+                    <div
+                        className="org-row"
+                        role="row"
+                        key={`${r.role}-${idx}`}
+                    >
+                        <div className="org-name" role="cell">
+                            {r.firstName} {r.lastName}
+                        </div>
+                        <div className="org-role" role="cell">
+                            {r.role}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
+}
